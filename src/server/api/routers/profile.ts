d51db70d86fb708e6c46f69b30e79c0d3eef9951
Aidan Timson <aidan@timmo.dev>
2024-09-14 19:03:05 +0100

    Add profile router

---
+++ src/server/api/routers/profile.ts
@@ -0,0 +1,71 @@
+import { eq } from "drizzle-orm";
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { profiles } from "~/server/db/schema";
+
+export const profileRouter = createTRPCRouter({
+  //
+  // Create
+  //
+  create: publicProcedure
+    .input(
+      z.object({
+        userId: z.string().min(1),
+        name: z.string().min(1),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.insert(profiles).values({
+        userId: input.userId,
+        name: input.name,
+      });
+    }),
+
+  //
+  // Read
+  //
+  get: publicProcedure
+    .input(
+      z.object({
+        userId: z.string().min(1),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const post = await ctx.db.query.profiles.findFirst({
+        where: (profiles, { eq }) => eq(profiles.userId, input.userId),
+      });
+
+      return post ?? null;
+    }),
+
+  //
+  // Update
+  //
+  updateName: publicProcedure
+    .input(
+      z.object({
+        userId: z.string().min(1),
+        name: z.string().min(1),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db
+        .update(profiles)
+        .set({ name: input.name })
+        .where(eq(profiles.userId, input.userId));
+    }),
+
+  //
+  // Delete
+  //
+  delete: publicProcedure
+    .input(
+      z.object({
+        userId: z.string().min(1),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.delete(profiles).where(eq(profiles.userId, input.userId));
+    }),
+});
